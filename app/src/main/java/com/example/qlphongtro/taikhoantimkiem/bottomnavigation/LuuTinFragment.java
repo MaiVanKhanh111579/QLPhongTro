6d8a5ee23feef439fce0b46e917f1cec99b4f633
hoangPhuc-hub <dangphuc7705@gmail.com>
2025-06-08 00:17:01 +0700

    Initial commit: thêm layout Lưu tin và vector drawable

--- app/src/main/java/com/example/qlphongtro/taikhoantimkiem/bottomnavigation/LuuTinFragment.java
+++ app/src/main/java/com/example/qlphongtro/taikhoantimkiem/bottomnavigation/LuuTinFragment.java
@@ -4,19 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.qlphongtro.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LuuTinFragment extends Fragment {
+    private ImageView ivEmptySaved;
+    private TextView tvEmptyTitle, tvEmptyDesc;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_luutin, container, false);
+
+        ivEmptySaved   = view.findViewById(R.id.iv_empty_saved);
+        tvEmptyTitle   = view.findViewById(R.id.tv_empty_saved_title);
+        tvEmptyDesc    = view.findViewById(R.id.tv_empty_saved_desc);
+
+        // Ví dụ: nếu bạn có list dữ liệu, check size để show/hide các view này
+        // if (savedList.isEmpty()) { … } else { … }
+
         return view;
     }
 }
